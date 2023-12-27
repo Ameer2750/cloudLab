33884adf9a10b0338c369c6141d615e5d5b550b3
Ameer <ameerbsc2630@gmail.com>
2023-12-27 19:31:31 +0530

    add form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -77,57 +77,54 @@
     </section>
 
     <!-- Ready to accelerate -->
-    <section class="">
-        <div>
-            <h2>Ready to Accelerate?</h2>
-            <p>
-                No matter where you are in your cloud journey, we can help you migrate, modernize, and manage your AWS
-                environment. Let’s accelerate your growth and fast-track your business outcomes.
+    <section class="bg-gray-200 py-14 px-14 flex flex-row gap-10 justify-between items-center">
+        <div class="flex flex-col gap-2 w-1/2">
+            <h2 class="text-2xl font-semibold">Ready to Accelerate?</h2>
+            <p class="font-thin leading-6">
+                No matter where you are in your cloud journey, we can help you migrate, modernize, <br>and manage your
+                AWS
+                environment. <br> Let’s accelerate your growth and fast-track your business outcomes.
             </p>
         </div>
-        <div>
-            <form action="">
-                <div>
+        <div class="w-1/2">
+            <form class="bg-white px-10 py-10 flex flex-col rounded gap-6" action="">
+                <div class="flex flex-col gap-2 w-3/4   ">
                     <label for="Email">Business Email</label>
-                    <input type="text">
+                    <input class="border border-gray-300 py-1 rounded" type="text">
                 </div>
-                <div>
-                    <div>
+                <div class="flex flex-row flex-wrap gap-6  ">
+                    <div class="flex flex-col gap-2 w-0.9/2">
                         <label for="FirstName*">First Name</label>
-                        <input type="text">
+                        <input class="border border-gray-300 py-1 rounded" type="text">
                     </div>
-                    <div>
+                    <div class="flex flex-col gap-2 w-0.9/2">
                         <label for="LastName">Last Name</label>
-                        <input type="text">
+                        <input class="border border-gray-300 py-1 rounded" type="text">
                     </div>
-                    <div>
+                    <div class="flex flex-col gap-2 w-0.9/2">
                         <label for="CompanyName">Company Name</label>
-                        <input type="text">
+                        <input class="border border-gray-300 py-1 rounded" type="text">
                     </div>
-                    <div>
+                    <div class="flex flex-col gap-2 w-0.9/2">
                         <label for="JobTitle">Job Title</label>
-                        <input type="text">
+                        <input class="border border-gray-300 py-1 rounded" type="text">
                     </div>
 
-                    <div>
-                        <label for="Email">Business Email</label>
-                        <input type="text">
+                    <div class="flex flex-col gap-2 w-0.9/2">
+                        <label for="City">City</label>
+                        <input class="border border-gray-300 py-1 rounded" type="text">
                     </div>
-                    <div>
-                        <label for="Email">Business Email</label>
-                        <input type="text">
-                    </div>
-                    <div>
-                        <label for="Email">Business Email</label>
-                        <input type="text">
+                    <div class="flex flex-col gap-2 w-0.9/2">
+                        <label for="State">State</label>
+                        <input class="border border-gray-300 py-1 rounded" type="text">
                     </div>
                 </div>
-                <div>
-                    <label for="Email">Business Email</label>
-                    <input type="text">
+                <div class="flex flex-col gap-2 w-3/4">
+                    <label for="Email">Phone number</label>
+                    <input class="border border-gray-300 py-1 rounded" type="text">
                 </div>
 
-                <button>Let's Go!</button>
+                <button class="bg-red-500 text-white px-3 py-2 mt-4 rounded self-end">Let's Go!</button>
             </form>
         </div>
     </section>
